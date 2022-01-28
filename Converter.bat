77f9e9d6dbf9aa872cdad9b9005f025b004baf8d
davFaithid <42425325+davFaithid@users.noreply.github.com>
2022-01-27 22:01:29 -0500

    First commit in 3 years
    First, changed the batch file so that the code is way more efficient (pursuant to issue #4 ). Secondly, updated the README. Finally, removed makerom.exe because I didn't make it, it's an outdated version (the latest release as of v0.18 works just fine), and it feels wrong to include it in this converter when you can just as easily download it.

--- Converter.bat
+++ Converter.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
-cd "%~dp0"
-echo Converting...
-makerom.exe -ciatocci "%~1"
-ren *.cci *.3ds
-echo Done!
-pause
+cd %~dp0
+for %%F in (%*) do (
+  makerom.exe -ciatocci %%F
+  ren "%%~dpnF.cci" "%%~nF.3ds"
+)
--- README.txt
+++
@@ -1,3 +0,0 @@
-Create a makerom folder. 
-Move makerom and Converter.bat to that folder
-Drag your CIA to Converter.bat and you are good!
